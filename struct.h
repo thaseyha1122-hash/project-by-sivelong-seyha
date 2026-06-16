2e88080abecf8a062a6f9cc6a03738ebdd2eee9f
seyha-sivelong <thaseyha1122@gmail.com>
2026-06-16 13:48:08 +0700

    Login

--- struct.h
+++ struct.h
@@ -5,3 +5,12 @@ typedef struct
     int phone_number;
     int password;
 }userLogin;
+
+
+typedef struct 
+{
+    char name[100];
+    char phone[100];
+    char gender[20];
+    char password[100];
+}userRegister;
--- userLogin.h
+++ userLogin.h
@@ -1,12 +1,20 @@
 #include "struct.h"
 #include<stdio.h>
 userLogin user;
-
+userRegister register;
 void login(){
 
-    printf("\n=============== Login from ============\n");
+    printf("\n=============== Login Form ============\n");
     printf("Enter Phone Number : ");
     scanf("%d",&user.phone_number);
     printf("Enter password : ");
     scanf("%d",&user.password);
+}
+
+void register(){
+    printf("\n =============== Register Form ============= \n");
+    printf("Enter Your Name : ");   scanf("%s",&register.name);
+    printf("Enter Your Gender : ");  scanf("%s",&register.gender);
+    printf("Enter Phone Number : ");    scanf("%s",&register.phone);
+    printf("Enter password : ");    scanf("%s",&register.password);
 }
