d79efd401b7a705b0b5ed26bbd07b529da5dde62
seyha-sivelong <thaseyha1122@gmail.com>
2026-06-16 13:35:29 +0700

    userlogin

--- userLogin.h
+++ userLogin.h
@@ -7,4 +7,6 @@ void login(){
     printf("\n=============== Login from ============\n");
     printf("Enter Phone Number : ");
     scanf("%d",&user.phone_number);
+    printf("Enter password : ");
+    scanf("%d",&user.password);
 }
