04c48d77489501fa22b53168559566b0c7eff976
seyha-sivelong <thaseyha1122@.com>
2026-06-20 22:18:41 +0700

    e

--- Admin/adminMenu.h
+++ Admin/adminMenu.h
@@ -5,29 +5,37 @@
 
 void adminMenu(){
     int choice;
-    printf("\n========== ADMIN MENU ==========\n");
-    printf("1. Add Car\n");
-    printf("2. View All Cars\n");
-    printf("3. Delete Car\n");
-    printf("4. View All Users\n");
-    printf("5. View All Bookings\n");
-    printf("6. Update Car\n");
-    printf("0. Logout\n");
+    do {
+        printf("\n========== ADMIN MENU ==========\n");
+        printf("1. Add Car\n");
+        printf("2. View All Cars\n");
+        printf("3. Delete Car\n");
+        printf("4. View All Users\n");
+        printf("5. View All Bookings\n");
+        printf("6. Update Car\n");
+        printf("0. Logout\n");
 
-    printf("Enter Your choice : ");    scanf("%d", &choice);
-    getchar();
+        printf("Enter Your choice : ");    scanf("%d", &choice);
+        getchar();
     
-    switch (choice) {
-    case 1:
-        addCar(); break;
-    // case 2:
-    //     viewAllcar();
-    //     break;
-    // case 3:
-    //     deleteCar();
-    default:
-        break;
-    }
+        switch (choice)
+        {
+            case 1:
+                addCar();
+                break;
+            // case 2:
+            //     viewAllcar();
+            //     break;
+            // case 3:
+            //     deleteCar();
+            case 0:
+                break;
+            default:
+                break;
+        }
+
+    } while(choice != 0);
+    
 }
 
 #endif
