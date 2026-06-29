7d18aed38f824fddc049e6cbc520863b9e44ff6d
seyha-sivelong <thaseyha1122@.com>
2026-06-29 12:10:01 +0700

    booking

--- Passanger/Booking.h
+++ Passanger/Booking.h
@@ -5,58 +5,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "../data.h"
 extern int carCount;
 extern Car cars[10];
+extern int loggedInuser;
+extern User users[100];
 
-void booking() {
+int car_index;
+
+void display_car_seats() {
+    LoadCarData();
     int id;
     printf("Enter car's ID( 0 : back ) : "); scanf("%d",&id);
     if (id == 0) {
         return;
     }
-    for(int i = 0; i <cars[id].TotalSeat ; i++){
+    int index;
+    for (int i=0; i<carCount; i++) {
+        if(id == cars[i].id) index = i;
+    }
+    for(int i = 0; i <cars[index].TotalSeat ; i++){
         if(i ==0 ){
-            printf(" [ D ]");
+            printf("\n[ D ]\n\n");
         }
-        if(cars->seatStatus[i] == 0){
-            printf("[ %d: O ]  ", i+1);
+        if(strcmp(cars[index].seatStatus[i], "0") == 0){
+            printf("[ %d ]\t", i+1);
         }
         else{
-            printf("[ %d: X ]  ", i+1);
+            printf("[ %s ]\t", cars[index].seatStatus[i]);
         }
         if((i+1) %  2 == 0){
             printf("\n\n");
         }
     }
-    // printf("Enter seat : ");
+    car_index = index;
+}
+
+void booking() {
+    LoadData();
+    display_car_seats();
+
+    printf("\n\n [D] = Driver\n [X] = Booked\n");
+
+    int index;
+    printf("Enter chair number to book : "); scanf("%d", &index);
+    //cars[car_index].seatStatus[index-1] = "1";
+    char c[2];
+    c[0] = users[loggedInuser].gender[0]; c[1] = '\0';
+    strcpy(cars[car_index].seatStatus[index - 1], c);
+    UpdateCarData();
 }
 
 
-    // for(int i = 0; i < c.totalSeat; i++)
-    // {
-
-    //     if(i == 0)
-    //     {
-    //         printf("[ D ]      ");
-    //         continue;
-    //     }
-
-
-    //     if(c.seatStatus[i] == 0)
-    //     {
-    //         printf("[ %d: O ]  ", i+1);
-    //     }
-    //     else
-    //     {
-    //         printf("[ %d: X ]  ", i+1);
-    //     }
-
-
-
-    //     if((i+1) % 2 == 0)
-    //     {
-    //         printf("\n\n");
-    //     }
-    // }
 
 #endif
